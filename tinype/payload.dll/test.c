8098f10b0a9ac355b5d4e9d4502ee096f2f73bb6
Garret Wassermann <gwassermann@cert.org>
2020-02-29 00:52:18 -0500

    add tinype submodule

--- tinype/payload.dll/test.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-int main()
-{
-    if (LoadLibrary("payload.dll") == NULL)
-        MessageBox(NULL, "Payload DLL failed to load", "Payload Test", MB_OK | MB_ICONERROR);
-
-    return 0;
-}
