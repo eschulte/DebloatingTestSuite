f1520aec098026e8bc2360830802ee91033800f1
Garret Wassermann <gwassermann@cert.org>
2020-04-27 04:15:21 -0400

    Create a second example scenario for chmod

---
+++ pytestbed/chmod_tests/ScenarioChmodNoCOption.py
@@ -0,0 +1,14 @@
+
+from pytestbed.TpcpUnitTest import TpcpTestCase, TpcpTestSuite
+import unittest
+
+# TODO: is there a way to automatically import scenario classes?
+
+from pytestbed.chmod_tests.TestChmodCOption import TestChmodCOption
+from pytestbed.chmod_tests.TestChmodEqualX import TestChmodEqualX
+
+def nocoptionScenario(path):
+    suite = TpcpTestSuite()
+    suite.addTest(TestChmodCOption(succeeds=False, exe=path))
+    suite.addTest(TestChmodEqualX(succeeds=True, exe=path))
+    return suite 
--- pytestbed/chmod_tests/__init__.py
+++ pytestbed/chmod_tests/__init__.py
@@ -5,9 +5,11 @@ import unittest
 # TODO: is there a way to automatically import scenario classes?
 
 from pytestbed.chmod_tests.ScenarioChmodStandard import standardScenario
+from pytestbed.chmod_tests.ScenarioChmodNoCOption import nocoptionScenario
 
 def load_tests(path):
     suite = standardScenario(path)
+    suite.addTest(nocoptionScenario(path))
     return suite
 
 
